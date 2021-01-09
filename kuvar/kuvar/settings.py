345b38b485c0473f2968a40fa6b80f94a170b077
Geralt43 <43779276+Geralt43@users.noreply.github.com>
2021-01-09 13:08:31 +0100

    Update settings.py

--- kuvar/kuvar/settings.py
+++ kuvar/kuvar/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+l%kqf7n-ayy1p)gh0)v$gxige(56z%i*))rg^kmlcur(bb@&$'
+SECRET_KEY = 'top secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
